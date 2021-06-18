5dc8660b580ac821e3379525f24d7d9f8d2f0f45
hro-maker <hrant.muradyan.1998@mail.ru>
2021-06-18 18:57:47 +0400

    l

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,18 +13,18 @@ import { EventsGateway } from './socket/message.gateway';
 import { EventsModule } from './socket/events.module';
 dotenv.config()
 @Module({
-imports:[EventsModule]
+    imports: [
+        ServeStaticModule.forRoot({rootPath: path.resolve(__dirname, 'static')}),
+          MongooseModule.forRoot(
+            `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.3l6j1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,{
+              useFindAndModify:false,
+            }
+          ),
+          AuthModule,
+          PostModule,
+          ComentModule,
+          MessageModule,
+             EventsModule
+        ],
 })
 export class AppModule {}
-// imports: [
-//   ServeStaticModule.forRoot({rootPath: path.resolve(__dirname, 'static')}),
-//     MongooseModule.forRoot(
-//       `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.3l6j1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,{
-//         useFindAndModify:false,
-//       }
-//     ),
-//     AuthModule,
-//     PostModule,
-//     ComentModule,
-//     MessageModule,
-//   ],
